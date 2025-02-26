91286c844846b86973c06e77cda599962b8fab2b
WillMaac <anderson.wjj@hotmail.com>
2025-02-26 17:37:27 -0300

    Conexão do back com o front

--- src/components/project/ProjectForm.js
+++ src/components/project/ProjectForm.js
@@ -1,22 +1,47 @@
-function ProjectForm(){
-    return (
-        <form>
-            <div>
-        <input type="text" placeholder="Insira o nome do projeto"/>
-            </div>
-            <div>
-        <input type="text" placeholder="Insira o orçamento total"/>
-            </div>
-            <div>
-        <select name="category_id">
-        <option disabled selected>Selecione a categoria</option>
-        </select>
-        </div>
-        <div>
-            <input type="submit" value= "Criar projeto" />
-        </div>
-        </form>
-    )
+import { useEffect, useState } from 'react';
+import Input from '../form/Input';
+import Select from '../form/Select';
+import SubmitButton from '../form/SubmitButton';
+
+function ProjectForm({btnText}) {
+
+    const [categories, setCategories] = useState ([])
+
+useEffect(() => {
+    fetch('http://localhost:5000/categories', {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+    })
+    .then((resp) => resp.json())
+    .then((data) => {
+        setCategories(data)
+    })
+}, [])
+    
+  return (
+    <form>
+    <Input
+       type= "text"
+       text = "Nome do projeto:"
+        name = "name"
+         placeholder= "Insira o nome do projeto" 
+         />
+     <Input
+       type= "number"
+       text = "Orçamento do projeto:"
+        name = "budget"
+         placeholder= "Insira o orçamento" 
+         />
+     <Select
+      name = "category_id"
+      text= "Selecione a categoria"
+      options={categories}
+      />
+      <SubmitButton text = {btnText} />
+    </form>
+  );
 }
 
 export default ProjectForm;
